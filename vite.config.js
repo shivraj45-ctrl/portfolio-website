6efe903e2e6a3f0b0b64655835d00bd61acecb2f
Shivraj Kudke <shivraj@example.com>
2026-07-02 21:16:08 +0530

    fix: add start_url to pwa manifest

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
         theme_color: '#000000',
         background_color: '#000000',
         display: 'standalone',
+        start_url: '/',
         icons: [
           {
             src: '/favicon.svg',
